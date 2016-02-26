febd0461826f42f4abae64ea933766602e454bc9
Sean Iams <sean.iams@gmail.com>
2016-02-26 14:50:50 -0800

    a working prototype;

--- app/js/color-picker-directive.js
+++ app/js/color-picker-directive.js
@@ -3,9 +3,22 @@ ColorPickerApp.directive("colorPicker", ["Settings", function(Settings) {
     restrict: 'E',
 
     link: function(scope, element, attrs, controller) {
+      scope.initialColor = Settings.initialColor;
+
       var jqElement = $(element);
-      var div = jqElement.find("div");
-      div.attr("style", "background-color: " + Settings.initialColor + ";");
+      var jqInput = jqElement.find("input#color-selection");
+      var jqValue = jqElement.find("#color-value");
+
+      jqInput.keyup(function(event) {
+        if (event.target.value.length === 0) {
+          jqValue.attr("style", "background-color: " + scope.initialColor + ";");
+        }
+        else {
+          jqValue.attr("style", "background-color: " + event.target.value + ";");
+        }
+      });
+
+      jqValue.attr("style", "background-color: " + scope.initialColor + ";");
     },
 
     templateUrl: "/html/partials/color-picker.html"
